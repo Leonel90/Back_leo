25eb8b4d3925a89f576b649641f4b7379d3623be
unknown <sleonel1990@gmail.com>
2023-10-26 21:42:05 -0500

    Subida desde Intellij casa

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Leonel Sangolquiza';
+    return 'Hello World! Leonel Omar S. A.';
   }
 }
